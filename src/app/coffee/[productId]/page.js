c27e1d7500f6d75b38e297cb9fa4c5c7f9dae16e
Frank3549 <fbautista@middlebury.edu>
2025-02-14 13:00:00 -0800

    added title and tasting notes description to the product page. Hard coded

--- src/app/coffee/[productId]/page.js
+++ src/app/coffee/[productId]/page.js
@@ -1,13 +1,24 @@
-
 "use client";
 import { useParams } from "next/navigation"
+import { useState } from "react";
 export default function ProductPage() {
     const {productId } = useParams();
-
+    const productData = {
+      name: "southern weather",
+      tastingNotes: ["milk chocolate", "plum", "candied walnuts", "juicy & citrus finish"],
+      price: 19,
+    };
+    const sizeOptions = ["4oz", "10oz", "2lb", "5lb"];
+    const [selectedSize, setSelectedSize] = useState(sizeOptions[1]); // default to 10oz
 
     return (
-      <div>
-        <h2>Product Page: {productId}</h2>
+        <div className="productPage">
+            <div className = "mainTitle">
+              <h1>{productData.name}</h1>
+              <p><span>{productData.tastingNotes.join(" | ")}</span></p>
+            </div>
         </div>
+
+
     );  
 }
