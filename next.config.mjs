4feac4546a9d09c9502989e353481273b4ca5014
Frank3549 <fbautista@middlebury.edu>
2025-02-14 22:40:00 -0800

    added title and photo of coffee with some styling so far.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'onyxcoffeelab.com',
+          pathname: '/cdn/shop/files/**',
+        },
+      ],
+    },
+  };
+  
+  export default nextConfig;
+  
--- src/app/coffee/[productId]/page.js
+++ src/app/coffee/[productId]/page.js
@@ -1,12 +1,14 @@
 "use client";
 import { useParams } from "next/navigation"
 import { useState } from "react";
+import Image from "next/image"; 
 export default function ProductPage() {
     const {productId } = useParams();
     const productData = {
       name: "southern weather",
       tastingNotes: ["milk chocolate", "plum", "candied walnuts", "juicy & citrus finish"],
       price: 19,
+      image: "https://onyxcoffeelab.com/cdn/shop/files/southernweather_1200x.webp?v=1724682333"
     };
     const sizeOptions = ["4oz", "10oz", "2lb", "5lb"];
     const [selectedSize, setSelectedSize] = useState(sizeOptions[1]); // default to 10oz
@@ -17,6 +19,17 @@ export default function ProductPage() {
               <h1>{productData.name}</h1>
               <p><span>{productData.tastingNotes.join(" | ")}</span></p>
             </div>
+
+            <div>
+              <Image 
+                className="productPagePhoto" 
+                src={productData.image} 
+                alt={productData.name + " image"} 
+                width={450} 
+                height={500}
+                />
+            </div>
+
         </div>
 
 
